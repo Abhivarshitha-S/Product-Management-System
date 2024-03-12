52e098a501c2f6156167852e0883b46f10d0d7ac
Abhivarshitha <116748885+Abhivarshitha-S@users.noreply.github.com>
2024-03-12 18:39:04 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+
+        ProductService service = new ProductService();
+
+        service.addProduct(new Product("microsoft","laptop","Brown table",2023));
+        service.addProduct(new Product("type c","cable","Black table",2026));
+        service.addProduct(new Product("blue pen","stationary","Brown table",2023));
+        service.addProduct(new Product("lenovo mouse","mouse","Black table",2023));
+        service.addProduct(new Product("samsung headset","headset","Brown table",2024));
+        service.addProduct(new Product("wd elements","hard disk","Brown table",2023));
+        service.addProduct(new Product("microsoft charger","cable","Brown table",2023));
+        service.addProduct(new Product("macbook pro","laptop","Black table",2025));
+        service.addProduct(new Product("microsoft mouse","mouse","Black table",2024));
+        service.addProduct(new Product("apple mouse","mouse","white table table",2023));
+
+       List<Product> products= service.getAllProducts();
+        /*
+        Enhanced "for loop" is being used
+         */
+        for(Product p: products){
+            System.out.println(p);
+        }
+        System.out.println("========================");
+        //printing specific product
+        System.out.println("a particular product: ");
+        Product p= service.getProduct("macbook pro");
+        System.out.println(p);
+
+        System.out.println("========================");
+        //printing specific text
+        System.out.println("a particular text: ");
+
+        List<Product>prods=service.getProductsWithText("mouse");
+        for(Product product: prods){
+            System.out.println(product);
         }
     }
 }
